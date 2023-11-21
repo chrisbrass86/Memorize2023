f1acc5e6339d2e96fd5bcdbf30eaf8e64c2043ec
Christoph Brass <mail@cbrass.de>
2023-11-21 19:01:43 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Christoph Braß on 21.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
